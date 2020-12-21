c9fd310410da2c24f04ffd550a69de9899b368a3
Caiua Gomes de Franca <caiua.franca@ale.com.br>
2020-12-21 11:16:14 -0300

    Ajuste na documentação de extract empresas

--- loads/extract/extract_empresas.py
+++ loads/extract/extract_empresas.py
@@ -5,15 +5,17 @@ import sqlite3
 conexaoSqlite = sqlite3.connect('../../files/dataMirrors/empresas.db')
 empresas = pd.read_sql_query("SELECT * FROM Empresas", conexaoSqlite)
 
-#empresas = pd.read_csv('../../files/spreadsheets/empresas.csv',sep=";")
 empresasCalc = pd.read_csv('../../files/spreadsheets/empresasCalc.csv',sep=";")
 
 dt_empresas = empresas.join(empresasCalc.set_index("cd_cnpj"), on="cd_cnpj")
 
 # Consulta para verificar
 # dt_empresas.query("cd_cnpj == 7297742000170")
+
+# Comando para dropar a coluna Index criada pelo pandas
 dt_empresas = dt_empresas.drop(columns=['index'])
 
+# ajustas os campos vazios do dataframe
 dt_empresas.fillna(0, inplace=True)
 
 #Exportando Dataframe para Mirrors
